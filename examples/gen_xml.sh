48ac335a653866498f9e49d483d442253faf4545
Bo Ferri <zazi@smiy.org>
2010-09-26 16:24:08 +0000

    property_reification: initial import

---
+++ examples/gen_xml.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Generate the RDF/XML from the Turtle code
+
+rapper -i turtle -o rdfxml-abbrev N3/prv_-_cco_example.n3 > RDF/prv_-_cco_example.rdf
---
+++ rdf/gen_xml.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Generate the RDF/XML from the Turtle code
+
+rapper -i turtle -o rdfxml-abbrev propertyreification.n3 > propertyreification.owl
+
+
