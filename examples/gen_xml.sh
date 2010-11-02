696ecfed323891194334a1632e926324b3e48194
Bo Ferri <zazi@smiy.org>
2010-11-02 10:14:06 +0000

    property_reification:  * ADDED: simple property reification example

--- examples/gen_xml.sh
+++ examples/gen_xml.sh
@@ -3,3 +3,4 @@
 # Generate the RDF/XML from the Turtle code
 
 rapper -i turtle -o rdfxml-abbrev N3/prv_-_cco_example.n3 > RDF/prv_-_cco_example.rdf
+rapper -i turtle -o rdfxml-abbrev N3/prv_-_simple_example.n3 > RDF/prv_-_simple_example.rdf
